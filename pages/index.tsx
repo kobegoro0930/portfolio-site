1288eb9f7745e74d50f0626a07a7e18ad78e9d25
kobegoro0930 <kobegoro@gmail.com>
2022-08-06 17:01:01 +0900

    Materiau UIのインストール

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import Link from "../node_modules/next/link";
+import Button from '@mui/material/Button';
+import { Typography } from "../node_modules/@mui/material/index";
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
 
 export default function Home() {
   return (
     <div>
-      <h1>TOPページ</h1>
+      <Typography variant="h1" p={3}>TOPページ</Typography>
       <ul>
         <li><Link href="/about">自己紹介</Link></li>
         <li><Link href="/skills">スキル</Link></li>
         <li><Link href="/posts">日々の記録</Link></li>
       </ul>
+      <Button variant="text"><AccessibilityIcon />Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   )
 }
