1fec1097f80787281a9d69cdf40c5847346c68e1
kobegoro0930 <kobegoro@gmail.com>
2022-08-21 21:12:03 +0900

    チャートコンポーネントの作成

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,17 +1,20 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-
-
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 const chartComponent = ({ data }) => {
   return (
-    <>
-      <LineChart width={600} height={300} data={data}>
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" />
+    <ResponsiveContainer width="100%" height={350}>
+      <LineChart
+        data={data}
+        margin={{ top: 55, right: 5, bottom: 0, left: 0 }}
+      >
+        <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="date" />
         <YAxis />
+        <Tooltip />
+        <Legend />
       </LineChart>
-    </>
+    </ResponsiveContainer>
   )
 };
 
--- pages/charts/index.tsx
+++ pages/charts/index.tsx
@@ -1,37 +1,77 @@
 import { client } from "../../libs/client";
 import dynamic from 'next/dynamic';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import { useEffect, useState } from "react";
+import { Typography, Container, Box } from "@mui/material";
+import Select from "react-select";
 
-// 動的インポート（非同期にモジュールを呼び出し）
-const ChartComponent = dynamic(() => import('../../components/chart'));
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-const Chart = () => {
-  const data = [
-    {name: 'Page A', pv: 10, amt: 2400},
-    {name: 'Page B', pv: 11, amt: 2400},
-    {name: 'Page C', pv: 13, amt: 2400},
-    {name: 'Page D', pv: 12, amt: 2400},
-  ];
+const ChartComponent = dynamic(() => import('../../components/Chart'));
+
+const Chart = ({ logs }) => {
+const processedData = logs.map(log => {
+    return {
+      menu: log.menu,
+      weight: log.weight,
+      date: dayjs.utc(log.publishedAt).tz('Asia/Tokyo').format('MM/DD')
+    }
+  }).reverse();
+
+  const chestTrainingData = processedData.filter(data => data.menu[0] === 'ダンベルフライ（胸筋）');
+  const bicepsTrainingData = processedData.filter(data => data.menu[0] === 'ダンベルカール（二頭筋）');
+  const tricepsTrainingData = processedData.filter(data => data.menu[0] === 'ダンベルプルオーバー（三頭筋）');
+
+  const [selectedMenu, setSelectedMenu] = useState();
+  const [selectedMenuTrainingData, setSelectedMenuTrainingData] = useState(chestTrainingData);
+
+  useEffect(() => {
+    switch (selectedMenu) {
+      case 'chest':
+        setSelectedMenuTrainingData(chestTrainingData);
+        break;
+      case 'biceps':
+        setSelectedMenuTrainingData(bicepsTrainingData);
+        break;
+      case 'triceps':
+        setSelectedMenuTrainingData(tricepsTrainingData);
+        break;
+    }
+    console.log(selectedMenu);
+  }, [selectedMenu])
+
+  const options = [
+    { value: 'chest', label: 'ダンベルフライ（胸筋）' },
+    { value: 'biceps', label: 'ダンベルカール（二頭筋）' },
+    { value: 'triceps', label: 'ダンベルプルオーバー（三頭筋）' }
+  ]
+
+  const handleSetSelectedMenu = (data) => {
+    setSelectedMenu(data);
+  }
+
   return (
-    <>
-      <h2>筋トレログ</h2>
-      <label>種目</label>
-      <select name="" id=""></select>
-      <br />
-      <label>期間</label>
-      <select name="" id=""></select>
-      〜
-      <select name="" id=""></select>
-      {/* {logs.map((log) => (
-        <div>
-          <p>{log.menu}</p>
-          <p>{log.weight}</p>
-        </div>
-      ))} */}
+    <Container maxWidth="lg">
+      <Typography variant='h4' component='h1' py={3}>
+        筋トレチャート
+      </Typography>
 
-      {/* グラフ描画 */}
-      <ChartComponent data={ data } />
+      <Typography component="span">種目</Typography>
 
-    </>
+      <Box sx={{ width: 300 }}>
+        <Select
+          options={options}
+          defaultValue={{label:'ダンベルフライ（胸筋）',value:'chest'}}
+          onChange={e => handleSetSelectedMenu(e.value)}
+        />
+      </Box>
+
+      <ChartComponent data={ selectedMenuTrainingData } />
+
+    </Container>
   )
 }
 
@@ -39,7 +79,7 @@ const Chart = () => {
 export const getServerSideProps = async () => {
   const data = await client.get({
     endpoint: "training_log",
-    queries: { limit: 100 }
+    queries: { limit: 200 }
   });
 
   return {
