b99407392c9690c27d691bcc398c0a2b310f86b8
kobegoro0930 <kobegoro@gmail.com>
2022-09-06 00:28:51 +0900

    チャート表記の修正

--- components/chart.tsx
+++ components/chart.tsx
@@ -7,7 +7,7 @@ const chartComponent = ({ data }) => {
         data={data}
         margin={{ top: 55, right: 20, bottom: 0, left: 0 }}
       >
-        <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+        <Line name="重量(kg)" type="monotone" dataKey="weight" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="date" />
         <YAxis />
